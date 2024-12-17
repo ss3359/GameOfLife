3a6b72feaaedc71360b20aa2c2b83eed1366e713
Stephen Sivetts <104328106+ss3359@users.noreply.github.com>
2024-12-17 16:45:44 -0700

    Add files via upload

--- GameOfLife.py
+++ GameOfLife.py
@@ -3,6 +3,7 @@ import math
 import pandas as pd
 import numpy as np
 import pygame
+import time
 
 
 #Initalize Pygame
@@ -29,53 +30,29 @@ pygame.display.set_caption("Game Of Life")
 CLOCK=pygame.time.Clock()
 
 #Variables
-grid=[[False for i in range(25)] for j in range(25)]
+# grid=[[False for i in range(25)] for j in range(25)]
 
 
 #Functions/Classes
-
-
-
-def FillInGrid(grid): 
-    for i in range(25): 
-        for j in range(0,24): 
-            I=random.randint(0,24)
-            J=random.randint(0,24)
-            grid[I][J]=True
-def FillInSquares(i,j): 
-    W=pygame.Rect(i,j,20,20)
-    pygame.draw.rect(WINDOW,WHITE,W,0); 
-def DrawEmptySquares(w,h): 
-    for i in range(0,w,20): 
-        for j in range(0,h,20): 
-            R=pygame.Rect(i,j,20,20)
-            pygame.draw.rect(WINDOW,WHITE,R)
-            pygame.draw.rect(WINDOW,BLACK,R)
-            pygame.draw.rect(WINDOW,RED,R,1)
-            
-def UpdateTheWindow(w,h): 
-     DrawEmptySquares(w,h)
-     pygame.display.update()
 
 class GameOfLife: 
     def __init__(self,r,c,g):
         self.rows=r
         self.columns=c
         self.generations=g
-        grid=[[]]
-        FillInGrid(grid)
-    def FillGrid(self,grid): 
+        self.grid=[[0 for j in range(self.columns)] for i in range(self.rows)]
+    def FillGrid(self): 
         for i in range(0,self.rows): 
             for j in range(0,self.columns): 
-                grid[i][j]=random.randint(0,1)
-        self.grid=grid
+                self.grid[i][j]=random.randint(0,1)
+        
 
     def DoAlgorithm(self,x,y): 
         alive=0
-        NewGrid=[[]]
-        for dx in range(-1,1): 
-            for dy in range(-1,1): 
-                if x==0 and y==0: 
+        NewGrid=[[0 for j in range(self.columns)] for i in range(self.rows) ]
+        for dx in range(-1,2): 
+            for dy in range(-1,2): 
+                if dx==0 and dy==0: 
                     continue
                 nx=x+dx
                 ny=y+dy
@@ -83,37 +60,58 @@ class GameOfLife:
                     if(self.grid[nx][ny]==0):
                         alive+=1
 
-        if grid[x][y]==0 and (alive < 2 or alive >3): 
-            NewGrid[x][y]=1
-        elif grid[x][y]==0 and (alive==2 or alive ==3): 
-            NewGrid[x][y]=0
-        elif grid[x][y]==1 and alive==3: 
-            NewGrid[x][y]=0
+        if self.grid[x][y]==1: 
+            if(alive<2 or alive>3):
+                NewGrid[x][y]=0
+            else:
+                NewGrid[x][y]=1
         else: 
-            NewGrid[x][y]=grid[x][y]
+            if alive==3:
+                NewGrid[x][y]=1        
         return NewGrid
 
+    def DrawGrid(self):
+        for x in range(self.rows): 
+                 for y in range(self.columns): 
+                     W=pygame.Rect(x*20,y*20,20,20)
+                     if self.grid[x][y]==1:
+                         pygame.draw.rect(WINDOW,WHITE,W)
+                         pygame.draw.rect(WINDOW,BLACK,W,1)
+                        
+                     else: 
+                       pygame.draw.rect(WINDOW,BLACK,W)
+                       pygame.draw.rect(WINDOW,RED,W,1)
+        pass
     def Generations(self):
         for n in range(self.generations): 
-             NewGrid=[[]]
+             NewGrid=[[0 for j in range(self.columns)] for i in range(self.rows)]
              for x in range(self.rows): 
                  for y in range(self.columns): 
                      NewGrid[x][y]=self.DoAlgorithm(x,y)[x][y]
-
              self.grid=NewGrid
-             
-  
-
-
-
+             self.DrawGrid()
+             pygame.display.update()
+              
+def FillInSquares(i,j): 
+    W=pygame.Rect(i,j,20,20)
+    pygame.draw.rect(WINDOW,WHITE,W,0); 
+def DrawEmptySquares(w,h): 
+    for i in range(0,w,20): 
+        for j in range(0,h,20): 
+            R=pygame.Rect(i,j,20,20)
+            pygame.draw.rect(WINDOW,BLACK,R)
+            pygame.draw.rect(WINDOW,RED,R,1)
+            
+def UpdateTheWindow(): 
+     g.Generations()
 
 #Run The Window
 run=True
+g=GameOfLife(25,25,500)
+g.FillGrid()
 
 while(run): 
-    CLOCK.tick(60)
-    FillInGrid(grid)
-
+    time.sleep(0.1)
     for event in pygame.event.get(): 
         if event.type==pygame.QUIT: 
              run=False
@@ -121,7 +119,7 @@ while(run):
             if event.key==pygame.K_ESCAPE: 
                 run=False 
     #Update The Window
-    UpdateTheWindow(WINDOWLENGTH,WINDOWHEIGHT)
+    UpdateTheWindow()
 #Exit Out Window
 pygame.quit()
             
